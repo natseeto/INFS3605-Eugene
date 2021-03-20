c40ea0c26b0e49964f2ff97abda735b3ff6af0b4
natseeto <61493185+natseeto@users.noreply.github.com>
2021-03-20 16:06:56 +1100

    Update MainActivity.java

--- app/src/main/java/com/example/infs3605/MainActivity.java
+++ app/src/main/java/com/example/infs3605/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     // test commit henry
+    
+    
+    // Implement Firebase logins + put in basic UI per BPMN - start connecting APIs
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
